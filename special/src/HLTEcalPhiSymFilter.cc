aeb0a6692a0c714113af9839139428b25e3449bf
Badder <badder.marzocchi@cern.ch>
2014-12-30 15:36:37 +0100

    HLTEcalPhiSymFilter

--- special/src/HLTEcalPhiSymFilter.cc
+++ special/src/HLTEcalPhiSymFilter.cc
@@ -13,6 +13,8 @@ HLTEcalPhiSymFilter::HLTEcalPhiSymFilter(const edm::ParameterSet& iConfig)
 {
   barrelDigis_ = iConfig.getParameter< edm::InputTag > ("barrelDigiCollection");
   endcapDigis_ = iConfig.getParameter< edm::InputTag > ("endcapDigiCollection");
+  barrelUncalibHits_ = iConfig.getParameter< edm::InputTag > ("barrelUncalibHitCollection");
+  endcapUncalibHits_ = iConfig.getParameter< edm::InputTag > ("endcapUncalibHitCollection");
   barrelHits_ = iConfig.getParameter< edm::InputTag > ("barrelHitCollection");
   endcapHits_ = iConfig.getParameter< edm::InputTag > ("endcapHitCollection");
   phiSymBarrelDigis_ = 
@@ -23,6 +25,8 @@ HLTEcalPhiSymFilter::HLTEcalPhiSymFilter(const edm::ParameterSet& iConfig)
     iConfig.getParameter< std::string > ("phiSymBarrelHitCollection");
   phiSymEndcapHits_ = 
     iConfig.getParameter< std::string > ("phiSymEndcapHitCollection");
+  ampCutADC_barl_ = iConfig.getParameter< double > ("ampCutADC_barrel");
+  ampCutADC_endc_ = iConfig.getParameter< double > ("ampCutADC_endcap");
   eCut_barl_ = iConfig.getParameter< double > ("eCut_barrel");
   eCut_endc_ = iConfig.getParameter< double > ("eCut_endcap");
   eCut_barl_high_=iConfig.getParameter< double > ("eCut_barrel_high");
@@ -33,6 +37,8 @@ HLTEcalPhiSymFilter::HLTEcalPhiSymFilter(const edm::ParameterSet& iConfig)
 
   barrelDigisToken_ = consumes<EBDigiCollection>(barrelDigis_);
   endcapDigisToken_ = consumes<EEDigiCollection>(endcapDigis_);
+  barrelUncalibHitsToken_ = consumes<EcalUncalibratedRecHitCollection>(barrelUncalibHits_);
+  endcapUncalibHitsToken_ = consumes<EcalUncalibratedRecHitCollection>(endcapUncalibHits_);
   barrelHitsToken_ = consumes<EBRecHitCollection>(barrelHits_);
   endcapHitsToken_ = consumes<EERecHitCollection>(endcapHits_);
 
@@ -52,10 +58,14 @@ HLTEcalPhiSymFilter::fillDescriptions(edm::ConfigurationDescriptions& descriptio
   edm::ParameterSetDescription desc;
   desc.add<edm::InputTag>("barrelDigiCollection",edm::InputTag("ecalDigis","ebDigis"));
   desc.add<edm::InputTag>("endcapDigiCollection",edm::InputTag("ecalDigis","eeDigis"));
+  desc.add<edm::InputTag>("barrelUncalibHitCollection",edm::InputTag("ecalUncalibHit","EcalUncalibRecHitsEB"));
+  desc.add<edm::InputTag>("endcapUncalibHitCollection",edm::InputTag("ecalUncalibHit","EcalUncalibRecHitsEE"));
   desc.add<edm::InputTag>("barrelHitCollection",edm::InputTag("ecalRecHit","EcalRecHitsEB"));
   desc.add<edm::InputTag>("endcapHitCollection",edm::InputTag("ecalRecHit","EcalRecHitsEE"));
   desc.add<unsigned int>("statusThreshold",3);
   desc.add<bool>("useRecoFlag",false);
+  desc.add<double>("ampCutADC_barrel",3.0);
+  desc.add<double>("ampCutADC_endcap",3.0);
   desc.add<double>("eCut_barrel",0.15);
   desc.add<double>("eCut_endcap",0.75);
   desc.add<double>("eCut_barrel_high",999999.);
@@ -84,11 +94,15 @@ HLTEcalPhiSymFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
   Handle<EBDigiCollection> barrelDigisHandle;
   Handle<EEDigiCollection> endcapDigisHandle;
+  Handle<EcalUncalibratedRecHitCollection> barrelUncalibRecHitsHandle;
+  Handle<EcalUncalibratedRecHitCollection> endcapUncalibRecHitsHandle;
   Handle<EBRecHitCollection> barrelRecHitsHandle;
   Handle<EERecHitCollection> endcapRecHitsHandle;
 
   iEvent.getByToken(barrelDigisToken_,barrelDigisHandle);
   iEvent.getByToken(endcapDigisToken_,endcapDigisHandle);  
+  iEvent.getByToken(barrelUncalibHitsToken_,barrelUncalibRecHitsHandle);
+  iEvent.getByToken(endcapUncalibHitsToken_,endcapUncalibRecHitsHandle);
   iEvent.getByToken(barrelHitsToken_,barrelRecHitsHandle);
   iEvent.getByToken(endcapHitsToken_,endcapRecHitsHandle);
  
@@ -99,6 +113,7 @@ HLTEcalPhiSymFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
   std::auto_ptr< EERecHitCollection > phiSymEERecHitCollection( new EERecHitCollection );
 
   //Select interesting EcalRecHits (barrel)
+
   EBRecHitCollection::const_iterator itb;
   for (itb=barrelRecHitsHandle->begin(); itb!=barrelRecHitsHandle->end(); itb++) {
     EBDetId ebid(itb->id());
@@ -108,18 +123,26 @@ HLTEcalPhiSymFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
       else statusCode = channelStatus[itb->id().rawId()].getStatusCode();
       if ( statusCode <=statusThreshold_ ) { 
            phiSymEBRecHitCollection->push_back(*itb);
-           EBDataFrame frameEB = (*barrelDigisHandle)[ebid.hashedIndex()];
-           phiSymEBDigiCollection->push_back(frameEB.id(),frameEB.frame().begin());
       }
       else if (itb->energy() >= eCut_barl_high_ ) {
 	   phiSymEBRecHitCollection->push_back(*itb);
-           EBDataFrame frameEB = (*barrelDigisHandle)[ebid.hashedIndex()];
-           phiSymEBDigiCollection->push_back(frameEB.id(),frameEB.frame().begin());
       }
+    }
+  }
+
+  EcalUncalibratedRecHitCollection::const_iterator itunb;
+  for (itunb=barrelUncalibRecHitsHandle->begin(); itunb!=barrelUncalibRecHitsHandle->end(); itunb++) {
+    EcalUncalibratedRecHit hit = (*itunb);
+    float amplitude = hit.amplitude();
+    EBDetId hitDetId = hit.id();
+    if( amplitude > ampCutADC_barl_){
+        EBDataFrame frameEB = (*barrelDigisHandle)[hitDetId.hashedIndex()];
+        phiSymEBDigiCollection->push_back(frameEB.id(),frameEB.frame().begin());
     }
   }
   
   //Select interesting EcalRecHits (endcaps)
+
   EERecHitCollection::const_iterator ite;
   for (ite=endcapRecHitsHandle->begin(); ite!=endcapRecHitsHandle->end(); ite++) {
     EEDetId eeid( ite -> id() );
@@ -129,14 +152,21 @@ HLTEcalPhiSymFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
        else statusCode =channelStatus[ite->id().rawId()].getStatusCode(); 
        if ( statusCode <=statusThreshold_ ) {
             phiSymEERecHitCollection->push_back(*ite);
-            EEDataFrame frameEE = (*endcapDigisHandle)[eeid.hashedIndex()];
-            phiSymEEDigiCollection->push_back(frameEE.id(),frameEE.frame().begin());
        }
        else if (ite->energy() >= eCut_endc_high_ ) {
 	    phiSymEERecHitCollection->push_back(*ite);
-            EEDataFrame frameEE = (*endcapDigisHandle)[eeid.hashedIndex()];
-            phiSymEEDigiCollection->push_back(frameEE.id(),frameEE.frame().begin());
        }
+    }
+  }
+
+  EcalUncalibratedRecHitCollection::const_iterator itune;
+  for (itune=endcapUncalibRecHitsHandle->begin(); itune!=endcapUncalibRecHitsHandle->end(); itune++) {
+    EcalUncalibratedRecHit hit = (*itune);
+    float amplitude = hit.amplitude();
+    EEDetId hitDetId = hit.id();
+    if( amplitude > ampCutADC_endc_){
+        EEDataFrame frameEE = (*endcapDigisHandle)[hitDetId.hashedIndex()];
+        phiSymEEDigiCollection->push_back(frameEE.id(),frameEE.frame().begin());
     }
   }
 
